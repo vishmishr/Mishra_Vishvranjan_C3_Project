a0dc4b21e07957a7e8ea33f57a7181922f9d252c
vishmishr <115252374+vishmishr@users.noreply.github.com>
2022-10-07 22:00:10 +0530

    Update RestaurantService.java

--- Restaurant_project -Student version/src/main/java/RestaurantService.java
+++ Restaurant_project -Student version/src/main/java/RestaurantService.java
@@ -5,8 +5,13 @@ import java.util.List;
 public class RestaurantService {
     private static List<Restaurant> restaurants = new ArrayList<>();
 
-    public Restaurant findRestaurantByName(String restaurantName){
-        return null;
+    public Restaurant findRestaurantByName(String restaurantName)  throws restaurantNotFoundException{
+         for(Restaurant rest : restaurants){
+            if(rest.getName().equals(restaurantName)){
+                return rest;
+            }
+        }
+        throw new restaurantNotFoundException(restaurantName);
         //DELETE ABOVE STATEMENT AND WRITE CODE HERE
     }
 
